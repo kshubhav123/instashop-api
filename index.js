134fc3486c7f96905e7eb6acaffcbe95cdafd779
kshubhav123 <kshubhav8@gmail.com>
2025-04-04 09:48:05 +0530

    updateindex

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+require("dotenv").config();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
